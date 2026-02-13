6374eff96884b57fda5c2619f44490aa98d7823b
Keeyong <keeyonghan@hotmail.com>
2026-02-12 18:00:26 -0800

    Remove 'dev.' prefix from session_summary queries

--- week4/worksheet-practice5.sql
+++ week4/worksheet-practice5.sql
@@ -1,20 +1,20 @@
 -- Checking for duplicate records
 SELECT COUNT(1)
-FROM dev.analytics.session_summary;
+FROM analytics.session_summary;
 
 SELECT COUNT(1)
 FROM (
     SELECT DISTINCT userId, sessionId, ts, channel
-    FROM dev.analytics.session_summary
+    FROM analytics.session_summary
 );
 
 -- Checking for the Presence of Recent Data (Freshness)
 SELECT MIN(ts), MAX(ts)
-FROM dev.analytics.session_summary;
+FROM analytics.session_summary;
 
 -- Checking for primary key uniqueness
 SELECT sessionId, COUNT(1)
-FROM dev.analytics.session_summary
+FROM analytics.session_summary
 GROUP BY 1
 ORDER BY 2 DESC
 LIMIT 1;
@@ -25,4 +25,4 @@ SELECT
    COUNT(CASE WHEN userId is NULL THEN 1 END) userid_null_count,
    COUNT(CASE WHEN ts is NULL THEN 1 END) ts_null_count,
    COUNT(CASE WHEN channel is NULL THEN 1 END) channel_null_count
-FROM dev.analytics.session_summary;
+FROM analytics.session_summary;
